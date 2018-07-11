677fa54afcff54269058c88cd86735240d8a0366
madforre <33324825+madforre@users.noreply.github.com>
2018-07-11 10:38:01 +0900

    view   수정
    간단한 폼양식 뷰 수정

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -18,7 +18,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('이메일 주소') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('이메일 주소를 적어주세요.') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,7 +64,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('가입하기') }}
                                 </button>
                             </div>
                         </div>
