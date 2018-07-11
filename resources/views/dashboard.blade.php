f18472ec777229eb4175ec9fc995b6d9c2f336f4
madforre <33324825+madforre@users.noreply.github.com>
2018-07-12 04:36:43 +0900

    dashboard 뷰 수정
    테이블 태그

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,13 +13,19 @@
                     <table class="table">
                         <tr>
                             <th>제목</th>
-                            <th class="right_side">수정하기</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th>수정하기</th>
                             <th>삭제하기</th>
                         </tr>
                     @foreach($posts as $post)
                         <tr>
                             <td>{{$post->title}}</td>
-                            <td><a href="/posts/{{$post->id}}/edit" class="right_side btn btn-outline-info">글수정</a></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-outline-info">글수정</a></td>
                             <td>
                                 <form action="{{route('posts.destroy', $post->id)}}" method="POST">
                                     @method('delete')
