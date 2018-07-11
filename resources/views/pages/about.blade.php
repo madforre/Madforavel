3444b495450131e49813b5e0c489e7216250cfb4
madforre <33324825+madforre@users.noreply.github.com>
2018-07-12 07:11:54 +0900

    view 수정
    About 페이지 띄어쓰기 태그 추가

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,6 +7,7 @@
     <p class="lead">This is the about page</p>
     <div class="about"><img src="/storage/images/whales.jpg" alt="whale_about"></div>
     <div class="about_text">
+        <br>
         Git-hub : <a href="https://github.com/madforre">github.com/madforre</a><br>
         2018-Concert-Life-Project : <a href="http://madforre.dothome.co.kr/">madforre.dothome.co.kr</a><br>
         Madforavel-AWS : <a href="http://ec2-13-209-49-129.ap-northeast-2.compute.amazonaws.com/">ec2-13-209-49-129.ap-northeast-2.compute.amazonaws.com</a>
