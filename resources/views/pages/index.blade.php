5b0bada7e80d0ffa9617149d646d0ccf55fb528a
madforre <33324825+madforre@users.noreply.github.com>
2018-07-12 19:07:27 +0900

    인덱스 테스트 계정추가
    뷰수정

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,6 +7,7 @@
           <img src="/storage/cover_images/bg18.jpg" alt="main.jpg"></img>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, culpa.</p>
           <p>매드포라벨에 오신 여러분을 환영합니다!</p>
+          <p>테스트 계정이 제공중입니다! ID : test4u@gmail.com / PW : test4u로 로그인 가능합니다!</p>
           <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login &raquo;</a>
           <a class="btn btn-success btn-lg" href="/register" role="button">Register &raquo;</a></p>
         </div>
